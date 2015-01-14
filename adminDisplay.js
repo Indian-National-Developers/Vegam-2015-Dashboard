7e8a736056d026af2faf4679169ff71e55e33507
saiy2k <saiy2k@gmail.com>
2015-01-14 18:15:20 +0530

    school based student retrieval

--- adminDisplay.js
+++ adminDisplay.js
@@ -1,97 +1,120 @@
-function loadSchoolFromParse(){
-                Parse.initialize("BlYcmQmAAvCsDdanA7TJh14KiHrCCqb3z5vPz1Ay", "ovGdJ7e9MJ0wqqcsadYauC9t5aiXvluiuqBrvf3x");
 
-                var UserClass = Parse.Object.extend("User");
-                var query = new Parse.Query(UserClass);
-                query.find({
-                    success: function(results) {
-                        userSelect  =   document.getElementById('userNameTextBox');
-                        results.forEach(function(user) {
-                            userSelect.options[userSelect.options.length] = new Option(user.get('username'), user.get('username'));
-                            console.log(user.get('username'));
-                        });
-                    }, error: function(error) {
-                    }
-                });
+var schoolObject = null;
+var studentsList = null;
 
-                $('#passwordTextBox').keypress(function(e) {
-                    if(e.which == 13) {
-                        loginButtonClick();
-                    }
-                });
-
-            }
-/*
-function studentRetrieval(){
-	var studClass = Parse.Object.extend("Student");
-	var query = new Parse.Query(studClass);
-	query.find({
-	success: function(studClass) {
-		console.log("Successfully retrieved " + studClass.length + " data.");
-    // Do something with the returned Parse.Object values
-    for (var i = 0; i < studClass.length; i++) { 
-      var object = studClass[i];
-      console.log(object.get('Username'));
-    }
-  },
-  error: function(error) {
-    alert("Error: " + error.code + " " + error.message);
-  }
+$( document ).ready(function() {
+    Parse.initialize("BlYcmQmAAvCsDdanA7TJh14KiHrCCqb3z5vPz1Ay", "ovGdJ7e9MJ0wqqcsadYauC9t5aiXvluiuqBrvf3x");
+    var UserClass = Parse.Object.extend("User");
+    var query = new Parse.Query(UserClass);
+    query.find({
+        success: function(results) {
+            userSelect  =   document.getElementById('userNameTextBox');
+            results.forEach(function(user) {
+                userSelect.options[userSelect.options.length] = new Option(user.get('username'), user.get('username'));
+            });
+        }, error: function(error) {
+        }
+    });
 });
-}			
-*/			
-function display(){
-				var User = Parse.Object.extend("User");
-				var query = new Parse.Query(User);
-				query.count({
-					success: function(count) {
-						// The count request succeeded. Show the count	
-						//document.getElementById("schoolnum").innerHTML = ("Number of Registered Schools: " + count);
-						//console.log(count);
-					},
-					error: function(error) {
-                        console.log("school count request faild");
-					}
-				});
-				var St = Parse.Object.extend("Student");
-				var squery = new Parse.Query(St);
-				squery.count({
-					success: function(count) {
-						// The count request succeeded. Show the count	
-						//document.getElementById("studnum").innerHTML = ("Number of Registered Students: " + count);
-						//console.log(count);
-					},
-					error: function(error) {
-                        console.log("school count request faild");
-					}
-				});
-            }
-			
-/*			
-function populateFields() {
-    var students = studentList;
-    var i = lindex-1;
-    var index       =   i+1;
-    var categ       =   students[i].get('CATEGORY');
-    $('#userNameField' + index).val(students[i].get('USERNAME'));
-    $('#rollNumberField' + index).val(students[i].get('ROLLNUMBER'));
-    $('#ageField' + index).val(students[i].get('AGE'));
-    $('#sexCombo' + index).val(students[i].get('GENDER'));
-    $('#categoryCombo' + index).val(categ);
-    $('#MRLevel' + index).hide();
-    if (categ == 'MR') {
-        $('#MRLevel' + index).val(students[i].get('LEVEL'));
-        $('#MRLevel' + index).show();
-    } else if (categ == 'VI') {
-        $('#VILevel' + index).val(students[i].get('LEVEL'));
-        $('#VILevel' + index).show();
-    }
-    isPopulating    =   true;
-    getEvents(index);
-    lindex++;
 
-    if (lindex <= students.length ) {
-        setTimeout(populateFields(), 1000);
-    }
-}
-*/
+function schoolStudentRetrieval(){
+
+    $('#studTable').empty();
+    var schoolName = document.getElementById('userNameTextBox').value;
+
+    var UserClass = Parse.Object.extend("User");
+    var query = new Parse.Query(UserClass);
+    query.equalTo('username', schoolName);
+    query.first({
+        success: function(obj) {
+            schoolObject    =   obj;
+            var stuClass=   Parse.Object.extend("Student");
+            var stQuery =   new Parse.Query(stuClass);
+            stQuery.equalTo("school", schoolObject);
+            stQuery.ascending("no");
+            stQuery.find({
+                success:function(students) {
+                    studentsList =   students;
+                    if (students.length) {
+                        populateTable();
+                    }
+                }, error:function(stobj, sterr) {
+                    console.log('Error fetching student' + sterr.message);
+                }
+            });
+        }, error:function(sobj, serr) {
+        }
+    });
+};
+
+function populateTable() {
+
+    $('#studTable').append(
+        "<tr>"+
+        "<th width='4%'> No </th>"+
+        "<th width='15%'> Name </th>"+
+        "<th width='10%'> Roll Number </th>"+
+        "<th width='4%'> Age </th>"+
+        "<th width='4%'> Gender </th>"+
+        "<th width='9%'> Disability </th>"+
+        "<th width='9%'> D.Level</th>"+
+        "<th width='20%'> Event 1 </th>"+
+        "<th width='20%'> Event 2 </th>"+
+        "</tr>");
+
+    studentsList.forEach(function(stud, i) {
+        var categ       =   stud.get('CATEGORY');
+        var levl        =   stud.get('LEVEL');
+        var index       =   i + 1
+        var levelString =   "";
+        if (categ == 'MR') {
+            levelString =   levl == "1" ? "mild" : levl == "2" ? "moderate" : "severe";
+        } else if (categ == 'VI') {
+            levelString =   levl == "1" ? "partially blind" : "totally blind";
+        }
+        $('#studTable').append(
+            "<tr id='studRow" + (i+1) + "'>"+
+            "<td> " + (i+1) + " </td>"+
+            "<td> " + stud.get('USERNAME') + " </td>"+
+            "<td> " + stud.get('ROLLNUMBER') + " </td>"+
+            "<td> " + stud.get('AGE') + " </td>"+
+            "<td> " + stud.get('GENDER') + " </td>"+
+            "<td> " + categ +  " </td>"+
+            "<td> " + levelString + " </td>"+
+            "<td id='studRowEventA" + (i+1) + "'> </td>"+
+            "<td id='studRowEventB" + (i+1) + "'> </td>"+
+            "</tr>");
+
+        var eventObject = Parse.Object.extend("EVENTS");
+
+        var eva = stud.get('event1');
+        if (eva != null) {
+            var evAQuery = new Parse.Query(eventObject);
+            evAQuery.get(eva.id, {
+                success: function(evv) {
+                    var strToDisp = evv.get('EVENT_NAME') + ' - ' + evv.get('TIME')
+                    $('#studRowEventA' + index).html(strToDisp);
+                },
+                error: function(object, error) {
+                }
+            });
+        }
+
+        var evb = stud.get('event2');
+        if (evb != null) {
+            var evBQuery = new Parse.Query(eventObject);
+            evBQuery.get(evb.id, {
+                success: function(evv) {
+                    var strToDisp = evv.get('EVENT_NAME') + ' - ' + evv.get('TIME')
+                    $('#studRowEventB' + index).html(strToDisp);
+                },
+                error: function(object, error) {
+                }
+            });
+        }
+
+    });
+
+};
+
+
